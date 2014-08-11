8186492077df4114d6b0f3951db942ef9e381c31
Jacob <jakeboone@aol.com>
2014-08-11 10:55:45 -0600

    first commit

---
+++ public/javascripts/bootstrap.js
@@ -0,0 +1,14 @@
+// $('#property a').click(function (e) {
+//   e.preventDefault()
+//   $(this).tab('show')
+// })
+
+// $('#bills a').click(function (e) {
+//   e.preventDefault()
+//   $(this).tab('show')
+// })
+
+// $('#footprint a').click(function (e) {
+//   e.preventDefault()
+//   $(this).tab('show')
+// })
---
+++ public/javascripts/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {                                  // When the document loads,
+    $('.location-entry[name=zipcode]').blur(function(){                        // Set on blur
+        $.zipLookup(                                            // Do a Zip code Lookup
+            $(this).val(),                                      // Zip code Field Value
+            function(cityName, stateName, stateShortName){      // If Successful,
+
+                $('.zip-info').html(cityName + ', ' + stateShortName);            // Add a message with the city/state
+
+            },
+            function(errMsg){                                   // If Zip couldn't be found,
+                $('.zip-info').html("Error: " + errMsg);         // Add an error message
+            });
+    });
+});
